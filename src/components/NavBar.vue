d9bf6339170261c11d2332e4ddfe94d9cf02fc95
runimo <63205791+runimo@users.noreply.github.com>
2025-04-22 23:54:25 +0200

    feat: add navigation bar
    - set '/orders' as default route
    - replace custom css classes with tailwind classes
    - remove unused logo

---
+++ src/components/NavBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface NavLink {
+  path: string;
+  name: string;
+}
+
+interface Props {
+  navLinks: NavLink[]
+}
+
+const { navLinks } = defineProps<Props>()
+</script>
+
+<template>
+    <nav class="w-full text-base text-center mt-8">
+        <RouterLink
+          v-for="link in navLinks"
+          :key="`navlink:${link.name.toLowerCase()}`"
+          activeClass="font-semibold text-purple-700 border-b-2 border-purple-700"
+          class="inline-block py-2 mx-2"
+          :to="link.path">
+          {{ link.name }}
+        </RouterLink>
+      </nav>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import OrdersList from '@/components/OrdersList.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      redirect: '/orders'
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-  ],
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      children: [
+        {
+          path: '/orders',
+          name: 'orders',
+          component: OrdersList,
+        },
+        {
+          path: '/products',
+          name: 'products',
+          component: () => import('../components/ProductsList.vue'),
+        }
+      ]
+    }
+  ]
 })
 
 export default router
