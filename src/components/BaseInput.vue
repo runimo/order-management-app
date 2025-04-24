046a23af3893ce0cdf4343687ad073458eeb5d79
runimo <63205791+runimo@users.noreply.github.com>
2025-04-24 18:57:07 +0200

    feat: add text input component

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+  defineEmits,
+  defineModel,
+  defineProps
+} from 'vue'
+
+const model = defineModel()
+
+defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  }
+})
+
+defineEmits([
+  'update:model-value'
+])
+</script>
+
+<template>
+  <div class="w-full">
+    <label
+      :for="id"
+      class="font-semibold cursor-pointer">
+      {{ label }}
+    </label>
+    <input
+      class="w-full border border-gray-300 rounded-xs mb-2 pl-2 h-[36px]"
+      :id="id"
+      type="text"
+      v-model="model" />
+  </div>
+</template>
