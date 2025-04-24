dc590092c649546e57494eea4580e4e2ab993442
runimo <63205791+runimo@users.noreply.github.com>
2025-04-24 18:58:52 +0200

    style: use consistent colors

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -14,14 +14,14 @@ const { navLinks } = defineProps<Props>()
 </script>
 
 <template>
-    <nav class="w-full text-base text-center mt-8">
-        <RouterLink
-          v-for="link in navLinks"
-          :key="`navlink:${link.name.toLowerCase()}`"
-          activeClass="font-semibold text-purple-700 border-b-2 border-purple-700"
-          class="inline-block py-2 mx-2"
-          :to="link.path">
-          {{ link.name }}
-        </RouterLink>
-      </nav>
+  <nav class="w-full text-base text-center mt-8">
+    <RouterLink
+      v-for="link in navLinks"
+      :key="`navlink:${link.name.toLowerCase()}`"
+      activeClass="font-semibold text-violet-700 border-b-2 border-violet-700 hover:text-violet-800 hover:border-violet-800"
+      class="inline-block py-2 mx-2"
+      :to="link.path">
+      {{ link.name }}
+    </RouterLink>
+  </nav>
 </template>
