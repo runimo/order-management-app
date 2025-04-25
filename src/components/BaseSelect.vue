b81170f439ff9f493714626182d6494edb1577d4
runimo <63205791+runimo@users.noreply.github.com>
2025-04-25 08:55:35 +0200

    feat: add select component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {
+  defineModel,
+  defineProps
+} from 'vue'
+
+const model = defineModel()
+
+defineProps({
+  id: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    default: ''
+  },
+  options: {
+    type: Array,
+    default: () => []
+  }
+})
+</script>
+
+<template>
+  <div class="w-full">
+    <label
+      class="font-semibold cursor-pointer block"
+      :for="id">
+      {{ label }}
+    </label>
+    <select
+      class="border border-gray-300 rounded-xs w-full h-[36px] cursor-pointer"
+      :id="id"
+      v-model="model">
+      <option
+        disabled
+        value="">
+        Please select
+      </option>
+      <option
+        v-for="option in options"
+        :key="option.id"
+        :value="option.id">
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
