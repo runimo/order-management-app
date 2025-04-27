8fc197c2c822c144c0e83d80a762bed34df1edb3
runimo <63205791+runimo@users.noreply.github.com>
2025-04-27 15:07:32 +0200

    feat: add logged in user info

--- src/App.vue
+++ src/App.vue
@@ -15,9 +15,16 @@ const navLinks = [
 </script>
 
 <template>
-  <header class="flex justify-center leading-loose max-h-screen">
-    <div class="flex items-start flex-wrap mb-8">
+  <header class="flex leading-loose max-h-screen">
+    <div class="flex justify-center w-full items-start flex-wrap mb-8">
       <NavBar :navLinks="navLinks" />
+    </div>
+    <div class="flex items-center">
+      <img
+        src="./assets/ashley-nicole-sVRW9DaK9ew-unsplash.jpg"
+        alt="User Photo"
+        title="You are logged in as User Name (Company 001)"
+        class="w-[52px] sm:w-12 h-12 rounded-full object-cover shadow-lg relative top-[-40px]" />
     </div>
   </header>
 
