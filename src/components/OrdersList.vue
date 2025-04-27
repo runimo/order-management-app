4b17a008459710bc3390ded51a6fa5a501155731
runimo <63205791+runimo@users.noreply.github.com>
2025-04-27 18:52:53 +0200

    refactor: move order create form to separate component

--- src/components/OrdersList.vue
+++ src/components/OrdersList.vue
@@ -7,6 +7,7 @@ import { useProductsStore } from '@/stores/products'
 import { useUserStore } from '@/stores/user'
 import BaseButton from './BaseButton.vue'
 import BaseSelect from './BaseSelect.vue'
+import CreateOrderForm from './CreateOrderForm.vue'
 import dayjs from 'dayjs'
 import ListActionFlyout from './ListActionFlyout.vue'
 import ProductQuantitySelector from './ProductQuantitySelector.vue'
@@ -17,7 +18,6 @@ const { currentUser } = userStore
 
 const ordersStore = useOrdersStore()
 const {
-  createOrder,
   deleteOrder,
   fetchOrders,
   resetOrderByType,
@@ -96,13 +96,7 @@ const { availableCompanies } = storeToRefs(companiesStore)
 const productsStore = useProductsStore()
 const { fetchProducts } = productsStore
 const availableProducts = computed(() => {
-  let supplierId = null
-
-  if (isEdit.value) {
-    supplierId = orderInEdit.value.supplierId
-  } else if (isCreateFormOpen.value) {
-    supplierId = newOrder.value.supplierId
-  }
+  const supplierId = orderInEdit.value.supplierId
 
   return supplierId ? productsStore.productsByCompanyId(supplierId) : []
 })
@@ -112,49 +106,9 @@ const formatDate = (isoString: string) => {
 }
 
 const isCreateFormOpen = ref(false)
-const createFormStep = ref(1)
-const steps = ['Select Supplier', 'Select Products', 'Confirm']
 const toggleCreateForm = (): void => {
   isCreateFormOpen.value = !isCreateFormOpen.value
 }
-const setCreateFormStep = (stepNumber: number): void => {
-  createFormStep.value = stepNumber
-}
-const cancel = (): void => {
-  toggleCreateForm()
-  resetCreateForm()
-}
-const handleBack = (): void => {
-  if (createFormStep === 2) {
-    newOrder.value = {
-      ...newOrder.value,
-      products: [],
-      totalPrice: 0
-    }
-  }
-
-  createFormStep.value--
-}
-const resetCreateForm = (): void => {
-  setCreateFormStep(1)
-  newOrder.value = {
-    supplierId: '',
-    products: [],
-    totalPrice: 0
-  }
-  resetOrderByType('orderBeingCreated')
-}
-const handleCreateOrder = (): void => {
-  toggleCreateForm()
-  createOrder()
-  resetCreateForm()
-}
-const options = computed(() => {
-  return availableCompanies.value.map(company => ({
-    id: company.id,
-    label: company.attributes.name
-  }))
-})
 
 const openFlyoutId = ref<string | null>(null)
 const toggleFlyout = (orderId: string): void => {
@@ -206,134 +160,10 @@ onMounted(() => {
         Create Order
       </BaseButton>
     </div>
-    <div
-      v-if="isCreateFormOpen">
-      <div class="flex justify-center">
-      <div class="w-[90%] md:w-[50%] md:justify-end rounded-xs border border-gray-300 mt-4 p-4">
-        <div class="flex items-center justify-center w-full max-w-3xl mb-[48px] mx-auto">
-          <div class="flex items-center w-[90%]">
-            <div
-              v-for="(step, index) in steps"
-              :key="index"
-              class="flex items-center relative"
-              :class="step === 'Confirm' ? '' : 'w-full'">
-              <div
-                :class="[
-                  'flex items-center justify-center w-8 h-8 rounded-full border-2 text-sm font-semibold z-10',
-                  createFormStep > (index + 1)
-                    ? 'bg-violet-600 border-violet-600 text-white'
-                    : createFormStep === (index + 1)
-                      ? 'bg-white border-violet-600 text-violet-600'
-                      : 'bg-white border-gray-300 text-gray-400']">
-                {{ index + 1 }}
-              </div>
-              <div
-                class="absolute top-10 text-xs text-center"
-                :class="[step === 'Confirm' ? 'left-[-3px]' : 'left-[-20px]', index + 1 === createFormStep ? 'font-semibold': '']">
-                {{ step }}
-              </div>
-              <div
-                v-if="index < steps.length - 1"
-                class="flex-1 h-1 mx-2 bg-gray-300 relative overflow-hidden">
-                <div
-                  class="h-1 bg-violet-600 absolute top-0 left-0 transition-all duration-500"
-                  :class="[createFormStep > (index + 1) ? 'w-full' : createFormStep === (index + 1) ? 'w-[50%]' : 'w-0']">
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div v-if="createFormStep === 1">
-          <BaseSelect
-            id="supplier"
-            label="Supplier"
-            :options="options"
-            v-model="newOrder.supplierId" />
-          <div class="flex flex-col sm:flex-row justify-end w-full mt-6">
-            <BaseButton
-              class="w-full sm:w-[30%] sm:mr-2"
-              variant="secondary"
-              @click="cancel">
-              Cancel
-            </BaseButton>
-            <BaseButton
-              class="w-full sm:w-[30%]"
-              :disabled="newOrder.supplierId === ''"
-              @click="setCreateFormStep(2)">
-              Next
-            </BaseButton>
-          </div>
-        </div>
-        
-        <div v-if="createFormStep === 2">
-          <ProductQuantitySelector
-            :available-products="availableProducts"
-            :order="newOrder"
-            orderType="orderBeingCreated" />
 
-          <div class="flex flex-col sm:flex-row justify-between w-full mt-6">
-            <BaseButton
-              class="w-full sm:w-[30%]"
-              variant="secondary"
-              @click="handleBack">
-              Back
-            </BaseButton>
-            <BaseButton
-              class="w-full sm:w-[30%]"
-              :disabled="newOrder.products.length === 0"
-              @click="setCreateFormStep(3)">
-              Next
-            </BaseButton>
-          </div>
-        </div>
-
-        <div v-if="createFormStep === 3">
-          <div>
-            <h2 class="font-semibold text-lg mb-2">Your order</h2>
-            <table class="w-full table-auto border-collapse">
-              <thead>
-                <tr>
-                  <th class="px-4 py-2 border border-gray-300 text-left">Product</th>
-                  <th class="px-4 py-2 border border-gray-300 text-left">Price</th>
-                  <th class="px-4 py-2 border border-gray-300 text-left">Quantity</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  v-for="product in newOrder.products"
-                  :key="product.id"
-                  class="border border-gray-300">
-                  <td class="px-4 py-2 border border-gray-300">{{ product.name }}</td>
-                  <td class="px-4 py-2 border border-gray-300">{{ product.price }} €</td>
-                  <td class="px-4 py-2 border border-gray-300">
-                    {{ product.count }}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-            <div class="text-right mt-4 mr-1">
-              <span class="font-semibold">Total: </span>
-              <span>{{ currentOrder.totalPrice }} €</span>
-          </div>
-          </div>
-
-          <div class="flex flex-col sm:flex-row justify-between w-full gap-2 mt-6">
-            <BaseButton
-              class="w-full sm:w-[30%]"
-              variant="secondary"
-              @click="handleBack">
-              Back
-            </BaseButton>
-            <BaseButton
-              class="w-full sm:w-[30%]"
-              @click="handleCreateOrder">
-              Create Order
-            </BaseButton>
-          </div>
-        </div>
-      </div>
-    </div>
-    </div>
+    <CreateOrderForm
+      v-if="isCreateFormOpen"
+      @close="toggleCreateForm()" />
 
     <div class="block md:hidden mt-8">
       <div
