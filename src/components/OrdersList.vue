f3f69def59aff87e98c461c70798b11c2957f625
runimo <63205791+runimo@users.noreply.github.com>
2025-04-27 16:13:12 +0200

    feat: add multistep form navigation

--- src/components/OrdersList.vue
+++ src/components/OrdersList.vue
@@ -112,6 +112,7 @@ const formatDate = (isoString: string) => {
 
 const isCreateFormOpen = ref(false)
 const createFormStep = ref(1)
+const steps = ['Select Supplier', 'Select Products', 'Confirm']
 const toggleCreateForm = (): void => {
   isCreateFormOpen.value = !isCreateFormOpen.value
 }
@@ -122,7 +123,18 @@ const cancel = (): void => {
   toggleCreateForm()
   resetCreateForm()
 }
-const resetCreateFormForm = (): void => {
+const handleBack = (): void => {
+  if (createFormStep === 2) {
+    newOrder.value = {
+      ...newOrder.value,
+      products: [],
+      totalPrice: 0
+    }
+  }
+
+  createFormStep.value--
+}
+const resetCreateForm = (): void => {
   setCreateFormStep(1)
   newOrder.value = {
     supplierId: '',
@@ -134,7 +146,7 @@ const resetCreateFormForm = (): void => {
 const handleCreateOrder = (): void => {
   toggleCreateForm()
   createOrder()
-  resetCreateFormForm()
+  resetCreateForm()
 }
 const options = computed(() => {
   return availableCompanies.value.map(company => ({
@@ -194,9 +206,42 @@ onMounted(() => {
       </BaseButton>
     </div>
     <div
-      v-if="isCreateFormOpen"
-      class="flex justify-center">
+      v-if="isCreateFormOpen">
+      <div class="flex justify-center">
       <div class="w-[90%] md:w-[50%] md:justify-end rounded-xs border border-gray-300 mt-4 p-4">
+        <div class="flex items-center justify-center w-full max-w-3xl mb-[48px] mx-auto">
+          <div class="flex items-center w-[90%]">
+            <div
+              v-for="(step, index) in steps"
+              :key="index"
+              class="flex items-center relative"
+              :class="step === 'Confirm' ? '' : 'w-full'">
+              <div
+                :class="[
+                  'flex items-center justify-center w-8 h-8 rounded-full border-2 text-sm font-semibold z-10',
+                  createFormStep > (index + 1)
+                    ? 'bg-violet-600 border-violet-600 text-white'
+                    : createFormStep === (index + 1)
+                      ? 'bg-white border-violet-600 text-violet-600'
+                      : 'bg-white border-gray-300 text-gray-400']">
+                {{ index + 1 }}
+              </div>
+              <div
+                class="absolute top-10 text-xs text-center"
+                :class="[step === 'Confirm' ? 'left-[-3px]' : 'left-[-20px]', index + 1 === createFormStep ? 'font-semibold': '']">
+                {{ step }}
+              </div>
+              <div
+                v-if="index < steps.length - 1"
+                class="flex-1 h-1 mx-2 bg-gray-300 relative overflow-hidden">
+                <div
+                  class="h-1 bg-violet-600 absolute top-0 left-0 transition-all duration-500"
+                  :class="[createFormStep > (index + 1) ? 'w-full' : createFormStep === (index + 1) ? 'w-[50%]' : 'w-0']">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div v-if="createFormStep === 1">
           <BaseSelect
             id="supplier"
@@ -205,10 +250,16 @@ onMounted(() => {
             v-model="newOrder.supplierId" />
           <div class="flex flex-col sm:flex-row justify-end w-full mt-6">
             <BaseButton
+              class="w-full sm:w-[30%] sm:mr-2"
+              variant="secondary"
+              @click="cancel">
+              Cancel
+            </BaseButton>
+            <BaseButton
               class="w-full sm:w-[30%]"
               :disabled="newOrder.supplierId === ''"
               @click="setCreateFormStep(2)">
-              Continue
+              Next
             </BaseButton>
           </div>
         </div>
@@ -219,12 +270,18 @@ onMounted(() => {
             :order="newOrder"
             orderType="orderBeingCreated" />
 
-          <div class="flex flex-col sm:flex-row justify-end w-full mt-6">
+          <div class="flex flex-col sm:flex-row justify-between w-full mt-6">
+            <BaseButton
+              class="w-full sm:w-[30%]"
+              variant="secondary"
+              @click="handleBack">
+              Back
+            </BaseButton>
             <BaseButton
               class="w-full sm:w-[30%]"
               :disabled="newOrder.products.length === 0"
               @click="setCreateFormStep(3)">
-              Continue
+              Next
             </BaseButton>
           </div>
         </div>
@@ -259,12 +316,12 @@ onMounted(() => {
           </div>
           </div>
 
-          <div class="flex flex-col sm:flex-row justify-end w-full gap-2 mt-6">
+          <div class="flex flex-col sm:flex-row justify-between w-full gap-2 mt-6">
             <BaseButton
               class="w-full sm:w-[30%]"
               variant="secondary"
-              @click="cancel">
-              Cancel
+              @click="handleBack">
+              Back
             </BaseButton>
             <BaseButton
               class="w-full sm:w-[30%]"
@@ -274,6 +331,7 @@ onMounted(() => {
           </div>
         </div>
       </div>
+    </div>
     </div>
 
     <div class="block md:hidden mt-8">
